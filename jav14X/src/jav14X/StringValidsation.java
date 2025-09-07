5c7c60a8bd61b528a1aa7074cd82902f2b9b1f52
milita <milit@192.168.1.8>
2025-09-07 19:42:05 +0530

    String class added

---
+++ jav14X/src/jav14X/StringValidsation.java
@@ -0,0 +1,12 @@
+package jav14X;
+
+public class StringValidsation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name ="Milita";
+		System.out.println(name);
+
+	}
+
+}
