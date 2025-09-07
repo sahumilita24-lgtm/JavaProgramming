3c27fa9270be92a71e44681f8134b8f4f9a7e896
milita <milit@192.168.1.8>
2025-09-07 22:26:02 +0530

    name refactor

--- jav14X/src/jav14X/StringValidation.java
+++ jav14X/src/jav14X/StringValidation.java
@@ -1,11 +1,20 @@
 package jav14X;
 
-public class StringValidsation {
+import java.util.ArrayList;
+
+public class StringValidation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String name ="Milita";
 		System.out.println(name);
+		ArrayList<String> marks = new ArrayList<String>();
+		marks.add("two");
+		marks.add("three");
+		for(String a: marks )
+		{
+			System.out.println(a + " :here is the mark");
+		}
 
 	}
 
