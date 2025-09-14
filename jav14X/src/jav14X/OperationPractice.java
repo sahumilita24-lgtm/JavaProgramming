880daa7eeabb79e5d73ec89ab9458d9f7610e4c1
milita <milit@192.168.1.8>
2025-09-14 22:28:02 +0530

    trail1 post pull

---
+++ jav14X/src/jav14X/OperationPractice.java
@@ -0,0 +1,13 @@
+package jav14X;
+
+public class OperationPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Unary
+		// binary
+		//Ternary
+
+	}
+
+}
