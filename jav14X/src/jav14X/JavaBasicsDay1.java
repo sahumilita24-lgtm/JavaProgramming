f176e723c3314a679c756c28a30a6e273d6b0204
milita <milit@192.168.1.8>
2025-09-14 22:25:07 +0530

    Trail1

--- jav14X/src/jav14X/JavaBasicsDay1.java
+++ jav14X/src/jav14X/JavaBasicsDay1.java
@@ -4,7 +4,23 @@ public class JavaBasicsDay1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//JavaBasicsDay1.printf_test();
+		int a = 20;
+        int b = 10;
+        System.out.println(a/b);
 
+
+	}
+	
+	public static void printf_test()
+	{
+		
+		int num =9;
+		System.out.printf("hello hi %h", "M");
+		
+	}
+	public static void comments()
+	{
 //		 Comments 1. single line 2. double line comments
 		/**
 		 * hello comment
@@ -15,7 +31,6 @@ public class JavaBasicsDay1 {
 
 		// Comment / uncomment with block comment Ctrl + Shift + /
 		// keyword and Identifiers
-
 	}
 
 }
