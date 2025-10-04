0beb1e4f4e2ae3037df91911e250a295b23a07f4
milita <milit@192.168.1.9>
2025-10-04 17:55:01 +0530

    function 04/10

---
+++ jav14X/src/ex13_Functions/Lab01_Functions.java
@@ -0,0 +1,14 @@
+package ex_13_Functions;
+
+import java.lang.Math;
+
+
+
+public class Lab128_Functions {
+    public static void main(String[] args) {
+        int max = Math.max(3, 4);
+        // Built - in
+
+        System.out.println(max);
+    }
+}
