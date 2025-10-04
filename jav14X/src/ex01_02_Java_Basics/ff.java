757803ef84a4e481ea539e82ae6b38a192fd2611
milita <milit@192.168.1.9>
2025-10-04 17:26:55 +0530

    qs

---
+++ jav14X/src/ex01_02_Java_Basics/ff.java
@@ -0,0 +1,75 @@
+package ex01_02_Java_Basics;
+
+public class ff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name1="FOX", name2="DOG"; if(name1 == "FOX") System.out.print("FOX "); System.out.println("GOOD"); if(name2 == "CAT") System.out.println("DINO");
+		String name="dino"; if(name == "dino") System.out.print("DINO"); System.out.println("GOOD");
+		// int marks=55; if(marks >= 80) System.out.println("DISTINCTION"); else if(marks >=35) System.out.println("PASS"); else System.out.println("FAIL");
+		// int marks=85; if(marks >= 80) System.out.println("DISTINCTION"); else if(marks >=35) System.out.println("PASS");
+		 float temp = 98.4f; if(temp > 98.4) { System.out.println("SUMMER"); } else { System.out.println("UNKNOWN"); }
+		 long num = 123L; if(num > 123) { System.out.println("TIGER"); } else { System.out.println("BIRD"); }
+		 //int horses = 10; int camels = 5; if(horses > 5) { if(camels > 3) { System.out.println("FOREST"); } } else { System.out.println("CITY"); }
+		 //int horses = 10; int camels = 5; if(horses < 5) { System.out.println("TOWN"); } else if(horses >=5) System.out.print("FOREST "); System.out.println("AMAZON"); else System.out.println("UNKNOWN");
+		 //int marks=29; if(marks > 29); System.out.print("PASS "); System.out.println("RANK");
+		 //if(true) { break; System.out.println("ELEPHANT"); }
+		 //if(x > 3) if(x < 10) System.out.print("A"); else System.out.print("B");
+		// int a = 0; if(a++) System.out.print("Hello"); else System.out.print("World");
+		 //int a = 10; if(a > 5) System.out.print("A"); else if(a > 7) System.out.print("B"); else System.out.print("C");
+		 //boolean flag = false; if(flag = true) System.out.print("Yes"); else System.out.print("No");
+		 //if(false) System.out.print("X"); System.out.print("Y");
+		 //int x = 7; if(x > 5) System.out.print("A"); System.out.print("B");
+		 //int n = 4; if(n % 2 == 0) System.out.print("Even"); else System.out.print("Odd");
+		 //int x = 10; if(x < 5) System.out.print("Small"); else if(x < 15) System.out.print("Medium"); else System.out.print("Large");
+		 
+		// int a = 5, b = 10; if(a > 0) if(b > 5) System.out.print("X"); else System.out.print("Y");
+		 //int a = 5, b = 2; if(a > 10) if(b > 1) System.out.print("A"); else System.out.print("B"); else System.out.print("C");
+		 //int x = 8; if(x > 5) if(x < 10) System.out.print("A"); else System.out.print("B"); else System.out.print("C");
+		 //int n = 6; if(n % 2 == 0) if(n % 3 == 0) System.out.print("Divisible by 6"); else System.out.print("Even but not by 3"); else System.out.print("Odd");
+		 //int x = 2; if(x == 2) if(x > 0) if(x < 5) System.out.print("Nested");
+		 //int a = 3; if(a > 0) if(a > 2) if(a > 4) System.out.print("A"); else System.out.print("B"); else System.out.print("C"); else System.out.print("D");
+		 //int x = 4; if(x > 0) if(x < 0) System.out.print("Impossible"); else System.out.print("Possible");
+		// int x = 1; if(x > 0) if(x > 2) System.out.print("A"); else if(x > -1) System.out.print("B"); else System.out.print("C"); else System.out.print("D");
+		// int a=1; while(a<4) { System.out.print(a + " "); a++; }
+		// int a=4; while(a>0) { System.out.print(a + " "); a--; }
+		 //String str="FOX"; int i=0; while(i<str.length()) { System.out.print(str.charAt(i)); i++; }
+		 //for(int i=1; i<5; i++) { System.out.print(i +","); }
+		 //int score=1; for(; true; score++) { System.out.print(score +","); if(score > 3) break; }
+		// for(int j=0; j<5;j++;) System.out.print(j + ",");
+		 
+		 
+		 
+		 
+		 String names[] = {"MOGLI", "SHAREKHAN", "BALU"}; for(String str: names) { System.out.print(str + ","); }
+		 
+		/* int age=20; do { age++; }
+		 while(age<20); 
+		 System.out.println("43" +age); */
+		 
+		 /* int i=1, j=1; while(i<3)
+		 { do 
+		 { System.out.print(j + ","); 
+		 j++; 
+		 }
+		 while(j<4); i++; }
+		 
+		 int time=50; do { System.out.print(time + ","); time++; }while(time < 53); */
+		 
+		// String str[] = {"A","B","C"}; int i=0; do { if(i>= str.length) break; System.out.print(str[i] + ","); i++; }while(true);
+		 
+		 // char ch[] = {'A', 'B', 'C'}; int i=0; do { System.out.print(ch[i] + ","); i++; }while(i < ch.length);
+		// int i=0; for(i=1; i<=6;i++) { if(i%3==0) continue; System.out.print(i+","); }
+		 
+		 
+	
+	
+	
+	}
+	
+	
+	
+	
+
+}
